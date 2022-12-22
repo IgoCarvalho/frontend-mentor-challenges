6f4bef33637faa0af1df16821f944dd03097a2ef
Igo Carvalho <igocarvalho00@gmail.com>
2022-12-22 18:57:12 -0300

    🎉 initial project setup

---
+++ multi-step-form/src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main>
+    <h1>Hello World</h1>
+  </main>
+</template>
+
+<style scoped>
+main {
+  display: grid;
+  place-content: center;
+}
+</style>

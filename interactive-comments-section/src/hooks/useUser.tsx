ca06786d25b6f723f43bf7e0d23ec6715d34c563
Igo Carvalho <igocarvalho00@gmail.com>
2023-02-06 21:35:51 -0300

    ✨ create User context

---
+++ interactive-comments-section/src/hooks/useUser.tsx
@@ -0,0 +1,42 @@
+import { createContext, ReactNode, useContext, useEffect, useMemo, useState } from 'react';
+
+import { User } from '../types/message';
+
+interface UserContextData {
+  user: User;
+  isCurrentUser: (user: User) => boolean;
+}
+
+const UserContext = createContext<UserContextData>({} as UserContextData);
+
+interface UserProviderProps {
+  children: ReactNode;
+}
+
+export function UserProvider({ children }: UserProviderProps) {
+  const [user, setUser] = useState<User>({} as User);
+
+  useEffect(() => {
+    fetch('/data.json')
+      .then((res) => res.json())
+      .then((data) => setUser(data.currentUser));
+  }, []);
+
+  function isCurrentUser(userToCompare: User) {
+    return userToCompare.username === user.username;
+  }
+
+  const context = useMemo(
+    () => ({
+      user,
+      isCurrentUser,
+    }),
+    [user]
+  );
+
+  return <UserContext.Provider value={context}>{children}</UserContext.Provider>;
+}
+
+export function useUser() {
+  return useContext(UserContext);
+}
